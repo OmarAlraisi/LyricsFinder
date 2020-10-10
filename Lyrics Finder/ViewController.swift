f6a6c370c21af25b681fafb179d2e3b17eea2960
Omar <o.raisi@hotmail.com>
2020-10-10 12:08:24 +0400

    Initial Commit

---
+++ Lyrics Finder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lyrics Finder
+//
+//  Created by Omar Al Raisi on 10/10/20.
+//  Copyright © 2020 Omar Al Raisi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
